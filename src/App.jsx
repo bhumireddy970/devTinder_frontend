3793b3952ec57c9549a3514d22a5919344754df3
Saradhi <bhumireddysaradhi9705@gmail.com>
2025-05-11 05:28:31 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Body from "./Body";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Login";
+import Register from "./Register";
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
